cee9ed43790ac5d9be735c39c84416ca89a2b757
Anderson <andersonjuninho2007@gmail.com>
2026-06-20 22:42:58 -0300

    reajust final(video)

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log("ola JS!");
 alert("vose foe haqueador. Sobrou nada ne but");
+const video = document.getElementById('playerElGato');
+
+if (video) {
+    // Tira o mudo automaticamente assim que o usuário clica no play
+    video.addEventListener('play', () => {
+        video.muted = false;
+        console.log("O vídeo do El Gato começou a tocar com som!");
+    });
+}
