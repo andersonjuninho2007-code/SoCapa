b88fa941bf77b72c243cc5715487038b96442381
Anderson <andersonjuninho2007@gmail.com>
2026-06-23 22:24:49 -0300

    atualiza comentários para melhor clareza no código

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 alert("vose foe haqueador. Sobrou nada ne but");
+// 1. Seleciona o elemento de vídeo e o botão
 const video = document.getElementById('playerElGato');
 const botton = document.getElementById('btnMostrarVideo');
 if (botton && video) {
     botton.addEventListener('click', () => {
-        // 1. Esconde o botão para não ficar poluindo a tela
+        // 2. Esconde o botão para não ficar poluindo a tela
         botton.style.display = 'none';
         
-        // 2. Mostra o player de vídeo na tela
+        // 3. Mostra o player de vídeo na tela
         video.style.display = 'block';
         
-        // 3. Garante que o som está ativo e inicia o vídeo
+        // 4. Garante que o som está ativo e inicia o vídeo
         video.muted = false;
         video.volume = 1.0;
         video.play();
