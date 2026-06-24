a89fddf2b52a36175b20b8eee24e19e8f5e2fed9
Anderson <andersonjuninho2007@gmail.com>
2026-06-23 22:18:41 -0300

    adição de um botao

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 alert("vose foe haqueador. Sobrou nada ne but");
 const video = document.getElementById('playerElGato');
-
-if (video) {
-    // Tira o mudo automaticamente assim que o usuário clica no play
-    video.addEventListener('play', () => {
+const botton = document.getElementById('btnMostrarVideo');
+if (botton && video) {
+    botton.addEventListener('click', () => {
+        // 1. Esconde o botão para não ficar poluindo a tela
+        botton.style.display = 'none';
+        
+        // 2. Mostra o player de vídeo na tela
+        video.style.display = 'block';
+        
+        // 3. Garante que o som está ativo e inicia o vídeo
         video.muted = false;
         video.volume = 1.0;
-        console.log("O vídeo do El Gato começou a tocar com som!");
+        video.play();
+        
+        console.log("O botão foi clicado, o vídeo apareceu e o som foi liberado!");
     });
 }
